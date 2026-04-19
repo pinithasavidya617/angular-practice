058e139d93092d5031ad4d5a69d81f025e6d67d2
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-04-19 09:46:03 +0530

    Add counter function,even odd function, ngif statements, ngModel and ngStyle. And add bmi calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  public counter = 0;
+  public evenOrOdd = '';
+  name = "John";
+  height = 0;
+  weight = 0;
+  bmi = 0;
+
+  constructor(){
+
+  }
+  public increment(){
+    this.counter += 1;
+    this.evenOdd();
+  }
+
+  public evenOdd(){
+    if (this.counter % 2 === 0){
+      this.evenOrOdd = 'Even';
+    }else{
+      this.evenOrOdd = 'Odd';
+    }
+  }
+
+  public bmiCal(){
+    const heightInMeters = this.height / 100;
+    this.bmi = this.weight / (heightInMeters ** 2);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentAComponent } from './component-a/component-a.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentAComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
