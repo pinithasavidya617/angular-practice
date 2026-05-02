91ba660f466d51f4a76e72637203fa009ee9ddd2
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-05-02 19:23:36 +0530

    Add ng for

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent {
+  public searchText = "";
+    public students = [
+      {name : "john", age: 25, address: "Colombo"},
+      {name : "nimal", age: 16, address: "Colombo"},
+      {name : "alice", age: 14, address: "Colombo"},
+      {name : "saman", age: 25, address: "Colombo"},
+      {name : "mammal", age: 89, address: "Colombo"},
 
+    ]
 }
